dc33f6d43c8c2a5c78cbb7b894067ffdaeeae73e
Conny Garpö <dolgan12@gmail.com>
2015-11-07 18:51:55 +0100

    Initialize repo

---
+++ server.js
@@ -0,0 +1,25 @@
+var port = 3000;
+var express = require('express');
+var app = express();
+
+var middleware = {
+    requireAuthentication: function (req, res, next){
+        console.log('private log hit!');
+        next();
+    },
+    logger: function (req, res, next){
+        console.log('Request:' + new Date().toUTCString() + ' ' + req.method + ' ' + req.originalUrl);
+        next();
+    }
+};
+
+app.use(middleware.logger);
+
+app.get('/about',middleware.requireAuthentication ,function (req, res) {
+    res.send('About Us');
+});
+
+app.use(express.static(__dirname + '/public'));
+app.listen(port, function () {
+    console.log('Express server started on port ' + port + '!');
+});
